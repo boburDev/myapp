243f08bf0cee8071d8c2af50a654d2ae667bf632
boburDev <negmatov1907@gmail.com>
2021-01-18 22:25:26 +0500

    add splash img

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Qale Jasur</Text>
+      <Text></Text>
       <StatusBar style="auto" />
     </View>
   );
